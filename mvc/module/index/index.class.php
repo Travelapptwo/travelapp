5ac949ac85f0be2a73777649b692e7c62846dcdd
dyl666 <1456122131@qq.com>
2017-07-10 12:04:21 +0800

    7.10dyl

--- mvc/module/index/index.class.php
+++ mvc/module/index/index.class.php
@@ -1,23 +1,38 @@
 <?php
 
-class index extends indexMain {
-    function init(){
-        $this->smarty->assign("login",$this->session->get("indexLogin"));
-        $this->smarty->assign("mname",$this->session->get("mname"));
+class index extends indexMain
+{
+    function init()
+    {
+        $this->smarty->assign("login", $this->session->get("indexLogin"));
+        $this->smarty->assign("mname", $this->session->get("mname"));
+
+        $db = new db("category");
+        $result = $db->where("pid=0")->select();
+        $this->smarty->assign("result", $result);
         $this->smarty->display("index.html");
     }
-    function code(){
-        $obj=new code();
-        $obj->codeUrl="demo.ttf";
-//        $obj->width=130;
-//        $obj->height=30;
+
+    function code()
+    {
+        $obj = new code();
+        $obj->codeUrl = "demo.ttf";
         $obj->outPut();
-        $this->session->set("code",$obj->current);
+        $this->session->set("code", $obj->current);
     }
-    function fenlei(){
+
+    function fenlei()
+    {
         $this->smarty->display("xiangqing.html");
     }
-    function xiangqing(){
+
+    function xiangqing()
+    {
         $this->smarty->display("xiangqing.html");
+    }
+
+    function sy_select()
+    {
+
     }
 }
